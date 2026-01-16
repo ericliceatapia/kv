01fc2b0dd149d340efa27ea13eb17561d860d08e
Eric Licea Tapia <ericliceatapia@outlook.com>
2026-01-15 22:56:28 -0600

    Clear function implemented

--- main.cpp
+++ main.cpp
@@ -23,13 +23,13 @@ int main(int argc, char *argv[]) {
     } else if (cmd.type == 'g') {
       std::cout << get(cmd.key) << std::endl;
     } else if (cmd.type == 'd') {
-        try {
-            del(cmd.key);
-        } catch (const std::out_of_range &e) {
-            std::cerr << e.what() << std::endl;
-        }
+      try {
+        del(cmd.key);
+      } catch (const std::out_of_range &e) {
+        std::cerr << e.what() << std::endl;
+      }
     } else if (cmd.type == 'c') {
-      std::cout << clr() << std::endl;
+      clr();
     } else if (cmd.type == 'a') {
       std::cout << all() << std::endl;
     }
